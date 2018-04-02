ac326078e3b0aa552c24c13caf8c1fde2a4cd3c1
柴田祐輝 <shibatayuki@shibatayuuakiranoMacBook-Air.local>
2018-04-02 11:40:48 +0800

    ホーム画面のモックアップ作成

---
+++ home.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<title>巡り野菜</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="assets/css/home.css">
+	</head>
+	<body>
+		<header>
+			<?php include("navbar.php"); ?>
+		</header>
+		<div class="top-wrapper">
+			<h1>巡り野菜</h1>
+		</div>
+		<div class="explain">
+			<div class="container">
+				<div class="explain-title">
+					<h2>捨てられる野菜を救おう</h2>
+				</div>
+				<div class="explain-text">
+				<p>毎日、食べる事ができるにも関わらず野菜たちが捨てられています。消費しきれなかったり沢山収穫し過ぎたり・・・。そんな野菜たちを救うためのサイトです。プロの農家さんでなくても、個人で家庭菜園されている方、野菜が余っている方でも気軽に野菜をあげる事ができます。</p>
+				</div>
+				<div class="btn-wrapper">
+					<div class="urikai">
+					<p>野菜が欲しい方</p>
+					<a class="btn product" href="">野菜一覧</a>
+					<p>野菜をもらった履歴</p>
+					<a class="btn buydata" href="">購入履歴</a>
+					</div>
+					<div class="rireki">
+					<p>野菜をあげたい方</p>
+					<a class="btn sell" href="">無料出品</a>
+					<p>野菜をあげた履歴</p>
+					<a class="btn selldata" href="">出品履歴</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<footer>
+			<div class="container">
+				<img class="footer-logo" src="assets/photos/logo.png">
+			</div>
+		</footer>
+	</body>
+</html>
