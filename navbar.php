bb92b48c8c31396da498761b5e0fbe19e7f86b01
柴田祐輝 <shibatayuki@shibatayuuakiranoMacBook-Air.local>
2018-04-02 11:43:24 +0800

    ナビ部分のinclude用ファイル

---
+++ navbar.php
@@ -0,0 +1,11 @@
+<div class="container">
+	<div class="header-left">
+		<img class="logo" src="assets/photos/logo.png">
+	</div>
+	<div class="header-login">
+		<a href="">ログイン</a>
+	</div>
+	<div class="header-signin">
+		<a href="">会員登録</a>
+	</div>
+</div>
