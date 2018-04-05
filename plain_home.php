60f65cc2026930f9e8fa87afeae3501059575a94
柴田祐輝 <shibatayuki@shibatayuuakiranoMacBook-Air.local>
2018-04-05 11:33:10 +0800

    旧home レスポンシブ未対応。

---
+++ plain_home.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<title>巡り野菜</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="assets/css/home.css">
+		<style type="text/css"><!--
+		/*ここに調整CSS記述*/
+		.jumbotron { background:url(../photos/vegetables.jpg) center no-repeat; background-size: cover;}
+		--></style>
+	</head>
+	<body>
+		<div class="conteiner-fluid">
+			<div class="row">
+				<div class="col-12">
+					<header>
+						<div class="container">
+							<div class="header-left">
+								<img class="logo" src="assets/photos/logo.png">
+							</div>
+							<div class="header-login">
+								<a href="">ログイン</a>
+							</div>
+							<div class="header-signin">
+								<a href="">新規登録</a>
+							</div>
+							<div class="header-mypage">
+								<a href="">マイページ</a>
+							</div>
+						</div>
+					</header>
+				</div>
+			</div>
+		</div>
+			<div class="conteiner">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="top-wrapper">
+						<h1>巡り野菜</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-offset-2">
+					<div class="explain">
+				<div class="container">
+					<div class="explain-title">
+						<h2>捨てられる野菜を救おう</h2>
+					</div>
+					<div class="explain-text">
+					<p>毎日、食べる事ができるにも関わらず野菜たちが捨てられています。消費しきれなかったり沢山収穫し過ぎたり・・・。そんな野菜たちを救うためのサイトです。プロの農家さんでなくても、個人で家庭菜園されている方、野菜が余っている方でも気軽に野菜をあげる事ができます。</p>
+					</div>
+					<div class="btn-wrapper">
+						<div class="urikai">
+						<p>野菜が欲しい方</p>
+						<a class="btn product" href="">野菜一覧</a>
+						<p>野菜をもらった履歴</p>
+						<a class="btn buydata" href="">購入履歴</a>
+						</div>
+						<div class="rireki">
+						<p>野菜をあげたい方</p>
+						<a class="btn sell" href="">無料出品</a>
+						<p>野菜をあげた履歴</p>
+						<a class="btn selldata" href="">出品履歴</a>
+						</div>
+					</div>
+				</div>
+			</div>
+				</div>
+			</div>
+		</div>
+		<footer>
+			<div class="container">
+				<img class="footer-logo" src="assets/photos/logo.png">
+			</div>
+		</footer>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	</body>
+</html>
