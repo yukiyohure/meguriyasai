e98a3347ef4b0f8bd7319429561d2c92ca8c302c
柴田祐輝 <shibatayuki@shibatayuuakiranoMacBook-Air.local>
2018-04-08 11:12:01 +0800

    出品履歴

---
+++ sell_data.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<!-- navbar -->
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<!-- localcss -->
+	<link rel="stylesheet" type="text/css" href="assets/css/sell_data.css">
+</head>
+<body>
+	<header>
+<!-- navbar -->
+		<nav class="navbar  navbar-inverse  navbar-fixed-top">
+			<div class="container">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="sr-only"> Toggle navigation</span>
+					<span class="icon-bar"> </span>
+					<span class="icon-bar"> </span>
+					<span class="icon-bar"> </span>
+				</button>
+				<a class="navbar-brand" href="#">巡り野菜</a>
+				<div class="navbar-collapse collapse">
+	        	<ul class="nav navbar-nav navbar-right">
+			     		<li><a href="#">会員登録</a></li>
+				 			<li><a href="#">ログイン</a></li>
+				 			<li><a href="#">マイページ</a></li>
+			   		</ul>
+       	</div>
+  		</div>
+		</nav>
+<!-- /.navbar -->
+	</header>
+	<div class="container">
+		<div class="table-responsive">
+			<table class="table table-bordered table-hover">
+				<caption class="text-center text-bold">出品履歴</caption>
+				<thead>
+					<tr>
+						<th>購入日時</th>
+						<th>品名</th>
+						<th>個数・数量</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>2018/07/07</td>
+						<td>茄子</td>
+						<td>12個</td>
+					</tr>
+					<tr>
+						<td>2018/07/01</td>
+						<td>ネギ</td>
+						<td>5本</td>
+					</tr>
+					<tr>
+						<td>2018/06/06</td>
+						<td>大根</td>
+						<td>10本</td>
+					</tr>
+					<tr>
+						<td>2018/04/18</td>
+						<td>オクラ</td>
+						<td>20本</td>
+					</tr>
+					<tr>
+						<td>2018/03/21</td>
+						<td>ジャガイモ</td>
+						<td>18個</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<div class="text-center">
+			<a class="btn btn-danger" href="#">トップへ戻る</a>
+		</div>
+	</div>
+	</div>
+	<footer>
+  	<div class="navbar  navbar-inverse navbar-fixed-bottom"> 
+	  	<div class="container">
+	      <div class="navbar-text pull=left">
+		    	<p> © meguriyasai 2018.</p>
+		   	</div>
+		 	</div>
+		</div>
+	</footer>
+
+<!-- navbar -->
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!-- bootstrap -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
