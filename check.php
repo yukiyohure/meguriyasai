07ebd010a7b4b4744706d934431976dfce0cbe66
柴田祐輝 <shibatayuki@shibatayuuakiranoMacBook-Air.local>
2018-04-10 16:57:45 +0800

    新規登録画面の確認画面

---
+++ check.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<!-- navbar -->
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<!-- localcss -->
+	<link rel="stylesheet" type="text/css" href="assets/css/check.css">
+</head>
+<body>
+	<header>
+<!-- navbar -->
+		<nav class="navbar  navbar-inverse  navbar-fixed-top">
+			<div class="container">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="sr-only"> Toggle navigation</span>
+					<span class="icon-bar"> </span>
+					<span class="icon-bar"> </span>
+					<span class="icon-bar"> </span>
+				</button>
+				<a class="navbar-brand" href="#">巡り野菜</a>
+				<div class="navbar-collapse collapse">
+	        	<ul class="nav navbar-nav navbar-right">
+			     	<li><a href="#">会員登録</a></li>
+				 		<li><a href="#">ログイン</a></li>
+				 		<li><a href="#">マイページ</a></li>
+			   		</ul>
+       	</div>
+  		</div>
+		</nav>
+<!-- /.navbar -->
+	</header>
+	<body style="margin-top: 60px">
+  <div class="container">
+    <div class="row box">
+      <div class="col-xs-8 col-xs-offset-2">
+        <h2 class="text-center content_header">アカウント情報確認</h2>
+        <div class="row">
+          <div class="col-xs-4">
+            <!-- <img src="../user_profile_img/<?php echo htmlspecialchars($img_name); ?>" class="img-responsive img-thumbnail"> -->
+            <img src="assets/photos/ironman.jpg">
+          </div>
+          <div class="col-xs-8">
+            <div>
+              <span>ユーザー名</span>
+              <p class="lead">ゆうき</p>
+            </div>
+            <div>
+              <span>メールアドレス</span>
+              <p class="lead">test@test.test</p>
+            </div>
+            <div>
+              <span>パスワード</span>
+              <!-- ② -->
+              <p class="lead">●●●●●●●●</p>
+            </div>
+            <!-- ③ -->
+            <form method="POST" action="">
+              <!-- ④ -->
+              <a href="signup.php?action=rewrite" class="btn btn-default">&laquo;&nbsp;戻る</a> | 
+              <!-- ⑤ -->
+              <input type="hidden" name="action" value="submit">
+              <input type="submit" class="btn btn-danger" value="ユーザー登録">
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+	<footer>
+  	<div class="navbar  navbar-inverse navbar-fixed-bottom"> 
+	  	<div class="container">
+	      <div class="navbar-text pull=left">
+		    	<p> © meguriyasai 2018.</p>
+		   	</div>
+		 	</div>
+		</div>
+	</footer>
+
+<!-- navbar -->
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!-- bootstrap -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
